932a8fbaa0e7adb4db74d4ae32d0ea81ee249a3f
rrroyal <royal@shameful.xyz>
2025-10-17 01:12:31 +0200

    Fix indents

--- Sources/PortainerKit/Types/API/Device.swift
+++ Sources/PortainerKit/Types/API/Device.swift
@@ -6,13 +6,13 @@
 //
 
 public struct Device: Codable, Equatable, Sendable {
-    enum CodingKeys: String, CodingKey {
-        case pathOnHost = "PathOnHost"
-        case pathInContainer = "PathInContainer"
-        case cgroupPermissions = "CgroupPermissions"
-    }
+	enum CodingKeys: String, CodingKey {
+		case pathOnHost = "PathOnHost"
+		case pathInContainer = "PathInContainer"
+		case cgroupPermissions = "CgroupPermissions"
+	}
 
-    public let pathOnHost: String?
-    public let pathInContainer: String?
-    public let cgroupPermissions: String?
+	public let pathOnHost: String?
+	public let pathInContainer: String?
+	public let cgroupPermissions: String?
 }
